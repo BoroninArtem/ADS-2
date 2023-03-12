f5fbc610ebce8f26940f3d846db48b876fa3e564
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-03-12 14:08:39 +0300

    b53

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,53 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-
-
 double pown(double value, uint16_t n) {
-
+    double sum = 1;
+    for (uint16_t i = 0; i < n; ++i) {
+        sum *= value;
+    }
+    return sum;
 }
 
 uint64_t fact(uint16_t n) {
-
+    if (n <= 1)
+        return 1;
+    return fact(n - 1) * n;
 }
 
 double calcItem(double x, uint16_t n) {
-
+    double num = 1, denom = 1;
+    for (uint16_t i = 1; i <= n; ++i) {
+        num *= x;
+        denom *= i;
+    }
+    return num / denom;
 }
 
 double expn(double x, uint16_t count) {
-
+    double sum = 1, c = 1;
+    for (uint16_t i = 1; i <= count; ++i) {
+        c *= x / i;
+        sum += c;
+    }
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
-
+    double sum = x, c = x;
+    for (uint16_t i = 1; i <= count; ++i) {
+        c *= -1 * x * x / (2 * i) / (2 * i + 1);
+        sum += c;
+    }
+    return sum;
 }
 
 double cosn(double x, uint16_t count) {
-
-}
+    double sum = 1, c = 1, znak = 1, fact = 1;
+    for (uint16_t i = 1; i <= count; ++i) {
+        znak *= -1.0;
+        fact *= 2.0 * i - 1.0;
+        c *= x * x / fact;
+        sum += znak * c;
+    }
+    return sum;
