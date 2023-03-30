ab8aea464ff960f9be56c12376c31b9bac9f231d
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-03-30 20:48:48 +0300

    b53

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,6 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-
 double pown(double value, uint16_t n) {
     double sum = 1;
     for (uint16_t i = 0; i < n; ++i) {
@@ -10,14 +9,12 @@ double pown(double value, uint16_t n) {
     return sum;
 }
 uint64_t fact(uint16_t n) {
-
     if (n <= 1)
         return 1;
     else
         return fact(n - 1) * n;
 }
 double calcItem(double x, uint16_t n) {
-
     double num = 1, denom = 1;
     for (uint16_t i = 1; i <= n; ++i) {
         num *= x;
@@ -26,7 +23,6 @@ double calcItem(double x, uint16_t n) {
     return num / denom;
 }
 double expn(double x, uint16_t count) {
-
     double sum = 1, c = 1;
     for (uint16_t i = 1; i <= count; ++i) {
         c *= x / i;
@@ -35,7 +31,6 @@ double expn(double x, uint16_t count) {
     return sum;
 }
 double sinn(double x, uint16_t count) {
-
     double sum = x, c = x;
     for (uint16_t i = 1; i <= count; ++i) {
         c *= -1 * x * x / (2 * i) / (2 * i + 1);
